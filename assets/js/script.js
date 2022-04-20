49f2a0984d3b07ed8d12246f08e222de8e5b9486
Bastiaan <ba_milo@hotmail.com>
2022-04-20 11:24:07 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+    
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivisionQuestion(){
+
+}
